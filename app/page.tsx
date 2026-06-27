133e868fb2f2d4e540e022915e33dd4179db7acb
yuto0730 <yuto.suzuki0730@gmail.com>
2026-06-27 18:17:48 +0900

    feat:検索フォームの実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ export default function Home() {
     <>
       <h1>ポケモン図鑑</h1>
       <h2>検索してみよう！</h2>
-      <p>名前を入力するとポケモンを検索できます。</p>
+      <p>名前を入力するとポケモンを検索できます。</p> 
+      <div>
+        <input 
+          type = 'text'
+          placeholder="ポケモンの名前を入力してください"
+        />
+        <button>検索</button>
+      </div>
     </>
   )   
 }
