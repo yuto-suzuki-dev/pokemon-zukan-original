4a4fc90e385113b92855aa3324bcbf521df45a40
yuto0730 <yuto.suzuki0730@gmail.com>
2026-06-28 10:01:42 +0900

    feat: ポケモン名の入力機能を実装

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,20 @@ export default function Home() {
       <h1>ポケモン図鑑</h1>
       <h2>検索してみよう！</h2>
       <p>名前を入力するとポケモンを検索できます。</p> 
+      
       <div>
         <input 
-          type="text"
+          value={pokemonName}
+          onChange={(e) => {
+            //入力したときに実行したい処理
+            setPokemonName(e.target.value);
+          }}
           placeholder="ポケモンの名前を入力してください"
         />
+      </div>
+      
+      <div>
+        <p>今あなたが入力した文字は {pokemonName} です</p>
         <button>検索</button>
       </div>
     </>
