e8373d02a443b0ebd885aa9f71f263d7c63e545a
yuto0730 <yuto.suzuki0730@gmail.com>
2026-07-04 13:49:45 +0900

    feat: ポケモン一覧カードを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,38 @@
-"use client";
-
-import {useState} from "react";
-
 export default function Home() {
-  const [pokemonName, setPokemonName] = useState("");
- 
+  const pokemons = [
+    { number: 1, name: "ピカチュウ", type: "でんき" },
+    { number: 2, name: "ヒトカゲ", type: "ほのお" },
+    { number: 3, name: "ゼニガメ", type: "みず" },
+    { number: 4, name: "フシギダネ", type: "くさ" }
+  ];
+
   return (
     <>
-      <h1>ポケモン図鑑</h1>
-      <h2>検索してみよう！</h2>
-      <p>名前を入力するとポケモンを検索できます。</p> 
-      <div>
-        <input 
-          type="text"
-          placeholder="ポケモンの名前を入力してください"
-        />
-        <button>検索</button>
+      <h1 className="font-bold">ポケモン図鑑</h1>
+      <button className="block py-2 w-20 border mb-5 bg-gray-50 mx-auto hover:bg-gray-100 cursor-pointer">検索する</button>
+
+      <div className="grid grid-cols-3 gap-6 w-full max-w-[1200px] mx-auto">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.number} className="border p-4 rounded hover:bg-gray-100 cursor-pointer">
+
+            <div className="h-80  mb-5 flex justify-center items-center">
+              <img src="/img/pa-ko.JPG"  alt="ポケモンの画像エリア" className="max-w-full max-h-full"/>
+            </div>
+            
+            <div className="text-center">
+              #{pokemon.number}
+            </div>
+            
+            <div className="font-bold text-center">
+              {pokemon.name}
+            </div>
+            
+            <div className="text-sm border rounded py-2 w-20 text-center mx-auto mt-2 mb-2">
+              {pokemon.type}
+            </div>
+
+          </div>
+        ))}
       </div>
     </>
   )   
