cb2d7e2ba646023b5b230736cd8b34178a58068b
yuto0730 <yuto.suzuki0730@gmail.com>
2026-07-07 20:02:06 +0900

    feat: ポケモンの図鑑番号を表示

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ interface ApiPokemon {
 
 export default async function Home() {
 
-  const responce = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30");
-
-  const data = await responce.json();
-  console.log(data);
-  const apiPokemons: ApiPokemon[] = data.results;
+  const responce = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30"); //pokeAPIからポケモンのデータを30件ずつ取得する
+  const data = await responce.json(); //取得したデータをjavascriptで使えるように変換しdataに入れる
+  console.log(data); //開発時、中身確認用
+  const apiPokemons: ApiPokemon[] = data.results; //取得して変換したポケモンのデータはapiPokemonsとする。apiPokemonsはインターフェースのApiPokemonのデータ型形式
 
   const pokemons = apiPokemons;
   console.log(apiPokemons[0]);
@@ -19,28 +18,36 @@ export default async function Home() {
       <h1 className="font-bold">ポケモン図鑑</h1>
       <button className="block py-2 w-20 border mb-5 bg-gray-50 mx-auto hover:bg-gray-100 cursor-pointer">検索する</button>
 
-      <div className="grid grid-cols-3 gap-6 w-full max-w-[1200px] mx-auto">
-        {pokemons.map((pokemon) => (
-          <div key={pokemon.url} className="border p-4 rounded hover:bg-gray-100 cursor-pointer">
+      <div className="grid grid-cols-3 gap-6 w-full max-w-[1200px] mx-auto">  {/* ポケモンのカード表示するエリア全体 */}
+        {pokemons.map((pokemon) => {
+          const urlParts = pokemon.url.split("/"); //APIから取得したポケモンのURLを"/"ごとに分解しurlPartsとする
+          console.log(urlParts); // 開発時、中身確認用
 
-            <div className="h-80  mb-5 flex justify-center items-center">
+          const pokemonNumber = urlParts[urlParts.length -2]; //urlPartsの中の配列の後ろから２番目のもの（番号）を取得し、pokemonNumberとする
+          
+
+          return (
+          <div key={pokemon.url} className="border p-4 rounded hover:bg-gray-100 cursor-pointer"> {/*ポケモンのカード単体*/}
+
+            <div className="h-80  mb-5 flex justify-center items-center"> {/*ポケモンの画像*/}
               <img src="/img/pa-ko.JPG"  alt="ポケモンの画像エリア" className="max-w-full max-h-full"/>
             </div>
             
-            <div className="text-center">
-              #{pokemon.url}
+            <div className="text-center"> {/*ポケモンの図鑑番号*/}
+              #{pokemonNumber}
             </div>
             
-            <div className="font-bold text-center">
+            <div className="font-bold text-center"> {/*ポケモンの名前*/}
               {pokemon.name}
             </div>
             
-            <div className="text-sm border rounded py-2 w-20 text-center mx-auto mt-2 mb-2">
+            <div className="text-sm border rounded py-2 w-20 text-center mx-auto mt-2 mb-2"> {/*ポケモンの説明文*/}
               未取得
             </div>
 
           </div>
-        ))}
+          )
+        })}
       </div>
     </>
   )   
