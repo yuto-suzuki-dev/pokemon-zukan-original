55023eccc99130ed7b82594a2720364da37f87a3
yuto0730 <yuto.suzuki0730@gmail.com>
2026-08-03 20:20:09 +0900

    feat: 詳細画面に画像を表示

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -13,12 +13,24 @@ export default async function PokemonDetailPage({ params }: DetailPageProps) {
   const responce = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const pokemonDetail = await responce.json();
 
+  // ポケモンの画像を取得
+  const pokemonImage = pokemonDetail.sprites.other["official-artwork"].front_default;
+
   // 開発時、中身確認用
   // console.log("ポケモンの詳細データは", pokemonDetail);
+  // console.log("ポケモンの画像は", pokemonImage);
 
   return (
     <>
       <h1>ポケモン詳細画面</h1>
+
+      <div className="flex justify-center my-5">
+        <img
+          src={pokemonImage}
+          alt={pokemonDetail.name}
+          className="w-80 h-80"
+        />
+      </div>
 
       <p>図鑑番号：{id}</p>
 
